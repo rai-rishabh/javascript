7446e5533c5985bd526107580490623e8673f868
Rishabh Rai <rairishabh871@gmail.com>
2024-08-04 08:38:51 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+//alert(3+3) //we are using nodejs, not browser so we get error as alert is not defined
+
+console.log(3+
+    3)          //code readability should be high
+
+console.log("Rishabh")
+
+
+let name = "Rishabh"
+let age = 23
+let isLoggedIn = false
+
+// number=> 2 to power 53
+// bigint
+// string => "" or '' but prefer using ""
+// boolean => true/false
+// null => let state = null //standalone value // Represents the intentional absence of any object value.
+// undefined => let state; //var declared but not initialized
+// symbol =>  to find uniqueness
+
+//object
+
+console.log(typeof "Rishabh");
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
